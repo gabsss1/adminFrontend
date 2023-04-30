def06f1b26ef0619816f6a03c16e78eefdab93df
gabsss1 <gventuram2015@gmail.com>
2023-04-30 10:33:34 -0500

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import {Routes, RouterModule} from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
+
+const appRoute: Routes = [
+
+]
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoute);
